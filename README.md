21fe3f796f84b4694a4679aa35907d3c864e2883
Rebecca McFadden <rmcfadden@tamu.edu>
2020-12-01 21:11:42 -0500

    gallery view albums

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cabin road photos app
+# cabinroadphotos
+
+A Flutter Photo Frame App - Works with Google Photos
---
+++ android/app/src/main/kotlin/com/example/cabinroadphotos2/MainActivity.kt
@@ -0,0 +1,6 @@
+package cabinroad.photos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
